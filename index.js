da8591587f88d0750325e9ab5a3c5af1e7d11614
Jared S <jaredjames2020@gmail.com>
2020-03-31 00:17:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ function unleashDog(dogName, dogBreed) {
   return unleashDog;
 }
 
-
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
 
 function exerciseDog(dogName, dogBreed) {
